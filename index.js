68ee87de19162133c598cfa6a8e9e6b643304421
Enes Buyukcelebi <enessalihbcelebi@fmail.com>
2023-01-01 12:14:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.querySelector('#main');
+element.remove(); 
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = " ENES is the champion";
